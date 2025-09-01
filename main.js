860d773b5015f4fff77681151b5fb0ed1e895de1
Benjamin <benjamin.sokolov19@gmail.com>
2025-09-01 23:00:18 +0300

    init: project

---
+++ main.js
@@ -0,0 +1,14 @@
+import { app, BrowserWindow } from "electron";
+
+const createNewWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createNewWindow();
+});
