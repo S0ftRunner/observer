9323b36e4e20b8d9fd56d61628caa93ceaf108fe
Benjamin <benjamin.sokolov19@gmail.com>
2025-10-14 22:49:18 +0300

    init: структура

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+
+// тут нужно запилить провайдер и роутинг
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ frontend/src/services/store.ts
@@ -0,0 +1,30 @@
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// Заготовка на будущий стор
+// import {
+//   TypedUseSelectorHook,
+//   useDispatch as dispatchHook,
+//   useSelector as selectorHook
+// } from 'react-redux';
+// import { ingredientsReducer } from './slices/ingredientSlice';
+// import { feedsReducer } from './slices/feedsSlice';
+// import { userOrderReducer } from './slices/userOrderSlice';
+// import { userDataReducer } from './slices/userDataSlice';
+
+// export const rootReducer = combineReducers({
+//   ingredients: ingredientsReducer,
+//   feeds: feedsReducer,
+//   userOrder: userOrderReducer,
+//   userData: userDataReducer
+// });
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+//   devTools: process.env.NODE_ENV !== 'production'
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+
+// export type AppDispatch = typeof store.dispatch;
+
+// export const useDispatch: () => AppDispatch = () => dispatchHook();
+// export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
